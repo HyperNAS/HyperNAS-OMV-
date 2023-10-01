96da102413d5d8fc61d1ce946652b8f80aec8c23
Jubian <jubian022@gmail.com>
2023-10-01 16:12:17 +0000

    Add build kernel function.

--- zang_bsp.sh
+++ zang_bsp.sh
@@ -1,18 +1,55 @@
 #!/bin/bash
 
+BSP_PATH=$PWD
+BASE_PATH=$PATH
+TOOLCHAINS_PATH=/home/sdkuser/rtd1296-toolchains/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/bin
+UBOOT_TOOLCHAIN_PATH=/home/sdkuser/rtd1296-toolchains/asdk64-4.9.4-a53-EL-3.10-g2.19-a64nt-160307/bin
+CON_PATH=$BASE_PATH:$TOOLCHAINS_PATH:$UBOOT_TOOLCHAIN_PATH
+KERNEL_PATH=/home/sdkuser/rtd1296-kernel
+PROCESSOR=`cat /proc/cpuinfo | grep processor | wc -l`
+
+function init()
+{
+	if [ ! -d rtd1296-kernel ];then
+            git clone git@gitee.com:styt_1/rtd1296-kernel.git
+    	else
+            cd rtd1296-kernel && git pull origin
+    	fi
+    	cd $BSP_PATH
+    	if [ ! -d rtd1296-toolchains ]; then
+            echo yes
+            git clone git@gitee.com:styt_1/rtd1296-toolchains.git
+    	else
+            echo no
+            cd toolchains && git pull origin
+    	fi
+    	cd $BSP_PATH
+    	sudo docker build --tag zang_1296:16.04 .
+}
+
+function make_kernel()
+{
+    	sudo docker run -it --rm --user sdkuser -v $BSP_PATH:/home/sdkuser \
+            	-e PATH=$CON_PATH \
+            	zang_1296:16.04 bash -c "cd $KERNEL_PATH && \
+            	make CROSS_COMPILE=aarch64-linux-gnu- ARCH=arm64 zang_nas_defconfig && \
+            	make CROSS_COMPILE=aarch64-linux-gnu- ARCH=arm64 -j$((PROCESSOR*2))"
+	if [ ! -d out ];then
+		mkdir -p out/kernel/dtb
+	else
+		rm -rf out/kernel/*
+		mkdir -p out/kernel/dtb
+	fi
+	cp rtd1296-kernel/arch/arm64/boot/Image out/kernel
+	cp -rf rtd1296-kernel/arch/arm64/boot/dts/realtek/rtd129x/*.dtb out/kernel
+}
+
 case "$1" in
   "init")
-    if [ ! -d rtd1296-kernel ];then
-	    git clone git@gitee.com:styt_1/rtd1296-kernel.git
-    else
-	    cd rtd1296-kernel && git pull origin
-    fi
-    if [ ! -d rtd1296-toolchains ]; then
-	    git clone git@gitee.com:styt_1/rtd1296-toolchains.git
-    else
-	    cd toolchains && git pull origin
-    fi
-    sudo docker build --tag zang_1296:16.04 .
+	  init
+    ;;
+  "kernel")
+	  make_kernel
     ;;
   *)
     echo "未知选项: $1"
